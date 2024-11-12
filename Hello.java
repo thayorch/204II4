42c22d3252408181bfc6d064578f779a38b7349f
thayorch <thatcharnon.10210@gmail.com>
2024-11-12 14:09:38 +0700

    eieiei

--- Hello.java
+++ Hello.java
@@ -1,9 +1,5 @@
-import java.util.Scanner;
-
 class Hello {
     public static void main(String[] args) {
-        
-        input = new Scanner
-        System.out.println("Hello World");
+        System.out.println("Alowwwww");
     }
 }
